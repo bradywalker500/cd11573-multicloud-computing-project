32e48df1920aa2a2ec58cf6452f5f71240a857fb
bradywalker500 <bradywalker500@gmail.com>
2023-08-01 18:52:35 +0100

    Correction: Declared undeclared resources

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -33,33 +33,33 @@ resource "azurerm_container_group" "udacity" {
 
 resource "azurerm_service_plan" "brads_service_plan" {
   name                = "bradsudacityserviceplan"
-  resource_group_name = data.azurerm_resource_group.example.name
-  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.brads_service_plan.name
+  location            = data.azurerm_resource_group.brads_service_plan.location
   sku_name            = "P1v2"
   os_type             = "Windows"
 }
 
 resource "azurerm_windows_web_app" "brads_windows_web_app" {
   name                = "bradsudacitywindowswebapp"
-  resource_group_name = data.azurerm_resource_group.example.name
-  location            = data.azurerm_service_plan.example.location
-  service_plan_id     = data.azurerm_service_plan.example.id
+  resource_group_name = data.azurerm_resource_group.brads_windows_web_app.name
+  location            = data.azurerm_service_plan.brads_windows_web_app.location
+  service_plan_id     = data.azurerm_service_plan.brads_windows_web_app.id
 
   site_config {}
 }
 
 resource "azurerm_storage_account" "brads_storage_account" {
   name                     = "bradsstorageaccount"
-  resource_group_name      = data.azurerm_resource_group.example.name
-  location                 = data.azurerm_resource_group.example.location
+  resource_group_name      = data.azurerm_resource_group.brads_storage_account.name
+  location                 = data.azurerm_resource_group.brads_storage_account.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_sql_server" "brads_sql_server" {
   name                         = "bradssqlserver"
-  resource_group_name          = data.azurerm_resource_group.example.name
-  location                     = data.azurerm_resource_group.example.location
+  resource_group_name          = data.azurerm_resource_group.brads_sql_server.name
+  location                     = data.azurerm_resource_group.brads_sql_server.location
   version                      = "12.0"
   administrator_login          = "mradministrator"
   administrator_login_password = "thisIsDog11"
