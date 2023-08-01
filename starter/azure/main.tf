d9158e263f2fa6fc028ae85efce86d3c108fb243
bradywalker500 <bradywalker500@gmail.com>
2023-08-01 18:15:18 +0100

    Added architecture diagram and respective cloud resources

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -1,5 +1,5 @@
 data "azurerm_resource_group" "udacity" {
-  name     = "Regroup_4gKqrgD_cn"
+  name     = "Regroup_6jpdjjmIdOgXVafVXW8J"
 }
 
 resource "azurerm_container_group" "udacity" {
@@ -30,3 +30,41 @@ resource "azurerm_container_group" "udacity" {
 }
 
 ####### Your Additions Will Start Here ######
+
+resource "azurerm_service_plan" "brads_service_plan" {
+  name                = "bradsudacityserviceplan"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  sku_name            = "P1v2"
+  os_type             = "Windows"
+}
+
+resource "azurerm_windows_web_app" "brads_windows_web_app" {
+  name                = "bradsudacitywindowswebapp"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_service_plan.example.location
+  service_plan_id     = data.azurerm_service_plan.example.id
+
+  site_config {}
+}
+
+resource "azurerm_storage_account" "brads_storage_account" {
+  name                     = "bradsstorageaccount"
+  resource_group_name      = data.azurerm_resource_group.example.name
+  location                 = data.azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_sql_server" "brads_sql_server" {
+  name                         = "bradssqlserver"
+  resource_group_name          = data.azurerm_resource_group.example.name
+  location                     = data.azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "mradministrator"
+  administrator_login_password = "thisIsDog11"
+
+  tags = {
+    environment = "production"
+  }
+}
